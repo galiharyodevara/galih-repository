deae4ef48b04d9b52fe015eeb0089c038d38479f
galiharyodevara <galiharyodevara@gmail.com>
2020-07-09 10:36:54 +0700

    Tugas Android

---
+++ app/src/main/java/com/example/splashscreen/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
