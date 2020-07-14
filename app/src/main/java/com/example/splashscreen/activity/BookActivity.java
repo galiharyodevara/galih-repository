5d835d9e70d59973ef5f9a418d3621a4d97bebc7
galiharyodevara <galiharyodevara@gmail.com>
2020-07-14 11:32:31 +0700

    AndroidApp Task

--- app/src/main/java/com/example/splashscreen/activity/BookActivity.java
+++ app/src/main/java/com/example/splashscreen/activity/BookActivity.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.splashscreen.R;
 
@@ -16,8 +18,29 @@ public class BookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
-        openHomeFragment();
+        Button btnHome;
+        Button btnAdd;
+        btnHome = findViewById(R.id.btnHome);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openHomeFragment();
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openSecondFragment();
+            }
+        });
     }
+
+
     private void openHomeFragment() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -25,4 +48,14 @@ public class BookActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.content, strCode,"home fragment");
         fragmentTransaction.commit();
     }
+
+    private void openSecondFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        SecondFragment strCode = new SecondFragment();
+        fragmentTransaction.replace(R.id.content, strCode,"second fragment");
+        fragmentTransaction.commit();
+    }
+
+
 }
